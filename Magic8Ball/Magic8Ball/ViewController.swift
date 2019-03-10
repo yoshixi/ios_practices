a16c4f57d83decf40e3749415567ba070c341f52
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2019-03-10 18:25:57 +0900

    create-project

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by yoshikimasubuchi on 2019/03/10.
+//  Copyright © 2019 yoshikimasubuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
